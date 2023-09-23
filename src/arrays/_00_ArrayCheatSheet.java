4dd2c53e621a9cee4b4f0b86f604b8764e0b4199
league <league@imac-16.attlocal.net>
2023-09-23 12:59:39 -0700

    array cheat sheet and robot race (not finished)

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -7,13 +7,16 @@ public class _00_ArrayCheatSheet {
 		//1. make an array of 5 Strings
 		String[] array = {"1","2","3","4","5"};
 		//2. print the third element in the array
-		System.out.println(array[3]);
+		System.out.println("Third element in array:");
+		System.out.println(array[2]);
 		//3. set the third element to a different value
 		array[2] = "hi";
 		//4. print the third element again
-		System.out.println(array[3]);
+		System.out.println("Updated third element of array:");
+		System.out.println(array[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		System.out.println("Whole array:");
 		for(int i=0; i<array.length; i++) {
 			System.out.println(array[i]);
 		}
@@ -22,6 +25,7 @@ public class _00_ArrayCheatSheet {
 		int [] intArray = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
 		Random r = new Random();
+		System.out.println("Array of 50 random integers:");
 		
 		for(int i=0; i<intArray.length; i++) {
 			
@@ -35,16 +39,23 @@ public class _00_ArrayCheatSheet {
 			if(intArray[i] < smallest) {
 				smallest = intArray[i];
 			}
-//			if(intArray[i] == 0) {
-//				
-//				intArray[i] = r.nextInt(10);
-//				System.out.println(intArray[i]);
-//				
-//			}
 		}
+		System.out.println("Smallest number in array:");
 		System.out.println(smallest);
 		//9 print the entire array to see if step 8 was correct
-		
+		System.out.println("Whole array:");
+		for(int i=1; i<intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
 		//10. print the largest number in the array.
+		System.out.println("Largest number in array:");
+		
+		int largest = intArray[5];
+		for(int i=1; i<intArray.length; i++) {
+			if(intArray[i] > largest) {
+				largest = intArray[i];
+			}
+		}
+		System.out.println(largest);
 	}
 }
